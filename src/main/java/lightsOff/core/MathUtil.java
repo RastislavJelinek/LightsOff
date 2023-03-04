c875ba2b0f209de6ad5ed8bfb107e350ba020f17
Rastislav <rasto@godis.sk>
2023-03-04 13:15:38 +0100

    more convenient range border, acting same on lowest and highest value

--- src/main/java/lightsOff/core/MathUtil.java
+++ src/main/java/lightsOff/core/MathUtil.java
@@ -3,6 +3,6 @@ package lightsOff.core;
 public class MathUtil {
     public static boolean inRange(int x, int min, int max)
     {
-        return x>=min && x<max;
+        return x>=min && x<=max;
     }
 }
